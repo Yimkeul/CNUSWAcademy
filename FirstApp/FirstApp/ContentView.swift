72a87e5646680c194d1b6f38bb41295776fa3515
YimKeul <leesjun29@gmail.com>
2023-01-02 13:41:14 +0900

    test

---
+++ FirstApp/FirstApp/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  FirstApp
+//
+//  Created by yimkeul on 1/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Button{ print("Clicked")} label:{Text("Hello Yimkeul")}
+        
+        
+    }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
+}
+ 
---
+++ FirstApp/FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by yimkeul on 1/2/23.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
