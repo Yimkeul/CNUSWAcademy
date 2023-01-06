45f6cd66fb2068313dcb4a7e7dfe2e57dcbe0966
YimKeul <leesjun29@gmail.com>
2023-01-06 17:51:57 +0900

    upload file

--- xcode-workspace/FirstApp/FirstApp/Object/MyStruct.swift
+++ xcode-workspace/FirstApp/FirstApp/Object/MyStruct.swift
@@ -7,17 +7,29 @@
 
 import SwiftUI
 
-//struct UserInfo {
-//
-//}
+struct UserInfo {
+    let name:String
+    let age :Int?
+    let email :String?
+    let job : String?
+    let hasPet : Bool
+    func sayMyName(with name : String) {
+        print(name)
+    }
+}
 
 struct MyStruct: View {
+    let userinfo : UserInfo
     var body: some View {
         VStack{
-            Text("Yimkeul")
-            Text("25")
+            Text(userinfo.name)
+            Text(userinfo.age?.description ?? "100")
+            Text(userinfo.email?.description ?? "test@test.com")
+            Text(userinfo.job?.description ?? "no")
+            Text(userinfo.hasPet.description)
+            
             Button {
-                sayMyName()
+                userinfo.sayMyName(with : userinfo.name)
             } label: {
                 Text("Button")
                 
@@ -26,13 +38,11 @@ struct MyStruct: View {
         }
         
     }
-    func sayMyName() {
-        print("Yimkeul")
-    }
 }
 
 struct MyStruct_Previews: PreviewProvider {
     static var previews: some View {
-        MyStruct()
+        let Sejun = UserInfo(name: "세준", age: 25, email: "leesjun29@gmail.com", job: "Student", hasPet: false)
+        MyStruct(userinfo:Sejun )
     }
 }
