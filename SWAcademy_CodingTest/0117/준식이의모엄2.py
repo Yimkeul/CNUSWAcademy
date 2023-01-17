523ba02f8ad134dc1cb6a73bf228eb7b9120310c
YimKeul <leesjun29@gmail.com>
2023-01-17 13:34:03 +0900

    upload

--- SWAcademy_CodingTest/0117/준식이의모엄2.py
+++ SWAcademy_CodingTest/0117/준식이의모엄2.py
@@ -13,32 +13,48 @@ U = row - 1
 D = row + 1
 """
 
-def doMove (ismap, controlMove , startRow, startCol , maxRow, maxCol) :
+def doMove (ismap,controlMove , startRow, startCol , maxRow, maxCol) :
     tempRow = startRow
     tempCol = startCol
-
+    hp = 0
     for i in controlMove:
         if i == "R":
             nc = tempCol +1
             if nc >=0 and nc < maxCol and ismap[tempRow][nc] != "#":
+                if ismap[tempRow][nc] == "^":
+                    hp +=1
                 tempCol = nc
-
+            else:
+                if ismap[tempRow][tempCol] == "^":
+                    hp +=1
         elif i == "L":
             nc = tempCol -1
             if nc >=0 and nc < maxCol and ismap[tempRow][nc] != "#":
+                if ismap[tempRow][nc] == "^":
+                    hp +=1                
                 tempCol = nc
-           
+            else:
+                if ismap[tempRow][tempCol] == "^":
+                    hp +=1                
         elif i == "U":
             nr = tempRow -1
             if nr >=0 and nr < maxRow and ismap[nr][tempCol] != "#":
+                if ismap[nr][tempCol] == "^":
+                    hp +=1                
                 tempRow = nr
-              
+            else:
+                if ismap[tempRow][tempCol] == "^":
+                    hp +=1                
         elif i == "D":
             nr = tempRow +1
             if nr >=0 and nr < maxRow and ismap[nr][tempCol] != "#":
+                if ismap[nr][tempCol] == "^":
+                    hp +=1                                
                 tempRow = nr
-           
-    print(tempRow+1,tempCol+1)    
+            else:
+                if ismap[tempRow][tempCol] == "^":
+                    hp +=1                
+    print(tempRow+1,tempCol+1,hp)    
 
 
 T = int(input())
