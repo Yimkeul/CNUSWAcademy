39d1abc307a5e997acfe8b53d1d681a6f232d9b9
YimKeul <leesjun29@gmail.com>
2023-01-26 17:43:16 +0900

    feat : screen UI Basic Step

--- xcode-workspace/MyAccountBook/MyAccountBook/ContentView.swift
+++ xcode-workspace/MyAccountBook/MyAccountBook/ContentView.swift
@@ -7,15 +7,89 @@
 
 import SwiftUI
 
+struct MainScrollView : View{
+    var body : some View{
+        ScrollView(){
+            ForEach(1..<100) {_ in
+                //                num in
+                //                Text("item \(num)").font(.title)
+                // 같은기능임
+                //                Text("Item \($0)").font(.title)
+                AccountRow()
+            }.frame(maxWidth: .infinity)
+                .background(.white)
+        }
+    }
+}
+
+struct AccountRow : View{
+    
+    var buttonArea : some View{
+        Button{}label: {
+            Text("+")
+        }
+    }
+    
+    var body : some View{
+        HStack{
+            // 로고자리
+            Text("💸")
+                .frame(width: 40, height: 40)
+                .background(.secondary)
+            Spacer()
+            // 타이틀
+            VStack{
+                Text("타이틀")
+                Text("금액")
+            }.background(.gray)
+            Spacer()
+            // Button 자리
+            buttonArea
+            
+        }
+    }
+}
+
+
+struct TopArea : View{
+    var body: some View{
+        HStack{
+            Button{
+                
+            }label: {
+                Text("Top Button")
+            }
+        }.padding()
+    }
+}
+
+struct BottomArea : View{
+    var body: some View{
+        HStack{
+            Button{
+                
+            }label: {
+                Text("button")
+            }
+        }
+    }
+}
+
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack
+        {
+            Color.gray.ignoresSafeArea().opacity(0.2)
+            VStack {
+                TopArea()
+                Spacer()
+                MainScrollView()
+                Spacer()
+                BottomArea()
+            }.padding()
         }
-        .padding()
+        
     }
 }
 
