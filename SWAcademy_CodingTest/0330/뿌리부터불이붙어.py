d9310e81dd4a93dac610eb9754e33506ec5f6f42
YimKeul <leesjun29@gmail.com>
2023-04-03 02:15:44 +0900

    뿌리 -62

--- SWAcademy_CodingTest/0330/뿌리부터불이붙어.py
+++ SWAcademy_CodingTest/0330/뿌리부터불이붙어.py
@@ -2,7 +2,7 @@ from collections import deque
 
 N = int(input())
 tree =[ [0]*(N+1) for _ in range(N+1)] 
-visited = [0]*(N+1)
+# visited = [0]*(N+1)
 # dist = [0] * (N+1)
 maxDistBfs = []
 
@@ -16,23 +16,27 @@ for i in range(N-1):
 def BFS(s):
     queue = deque()
     queue.append(s)
+    visited = [0]*(N+1)
     visited[s] = 1
     dist= [0]*(N+1)
     while queue:
         pop  = queue.popleft()
+        # print(f" {pop}", end = " ")
         for i in range(1,N+1):
             if visited[i] == 0 and tree[pop][i] == 1:
                 queue.append(i)
                 dist[i] = dist[pop] +1
                 visited[i] = 1
+        # print(dist)
     return max(dist)    
+
     
 
 
 for i in range(1,N+1):
-    # print(f"{i}번째 , 값{dist} , {max(dist)}")
+    # print(f"{i}번째 , 값{BFS(i)} ")
     maxDistBfs.append(BFS(i))
-    visited = [0]*(N+1)
+    # visited = [0]*(N+1)
     
 
 
